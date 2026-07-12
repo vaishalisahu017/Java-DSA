81bca988e2b3a705fbc650101d0e2512b57d3c79
vaishalisahu017 <vaishali.sahu017@gmail.com>
2026-07-12 19:43:19 +0530

    Create MergeSorted.java

---
+++ Arrays/MergeSorted.java
@@ -0,0 +1,24 @@
+class Solution {
+      public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+    int[] temp = new int[m+n];
+    int i=0, j=0, k=0;
+
+    while(i<m && j<n){
+        if(nums1[i] <= nums2[j])
+            temp[k++] = nums1[i++];
+        else
+            temp[k++] = nums2[j++];
+    }
+
+    while(i<m)
+        temp[k++] = nums1[i++];
+
+    while(j<n)
+        temp[k++] = nums2[j++];
+
+    for(int x=0; x<m+n; x++)
+        nums1[x] = temp[x];
+}
+  
+    }
