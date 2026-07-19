e43a2bb65c34bb0a271cb33aa3c1ccd555437bd2
vaishalisahu017 <vaishali.sahu017@gmail.com>
2026-07-19 20:16:01 +0530

    Create pivotIndex.java

---
+++ Arrays/pivotIndex.java
@@ -0,0 +1,19 @@
+class Solution { 
+ public int pivotIndex(int[] nums) {
+    int total = 0;
+    for (int n : nums) total += n;
+
+    int left = 0;
+
+    for (int i = 0; i < nums.length; i++) {
+        int right = total - left - nums[i];
+
+        if (left == right)
+            return i;
+
+        left+= nums[i];
+    }
+    return -1;
+}
+}       
+    
