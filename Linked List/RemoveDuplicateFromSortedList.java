aa1c2fb589d972e251b336a1a5b8ca46573f3ac4
Vaishali Sahu <vaishali.sahu017@gmail.com>
2026-08-05 18:06:29 +0530

    Create RemoveDuplicateFromSortedList.java

---
+++ Linked List/RemoveDuplicateFromSortedList.java
@@ -0,0 +1,19 @@
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+         if (head == null) {
+        return null;
+    }
+
+    ListNode current = head;
+    while (current != null && current.next != null) {
+
+        if (current.val == current.next.val) {
+            current.next = current.next.next;
+        } else {
+            current = current.next;
+        }
+    }
+    return head;
+}
+    
+}
