6604035ed61b08c4a9fdcf9d6442d0e31df50a85
Vaishali Sahu <vaishali.sahu017@gmail.com>
2026-08-19 20:48:29 +0530

    Create ImplimentStackUsingQueues.java

---
+++ Queue/ImplimentStackUsingQueues.java
@@ -0,0 +1,27 @@
+class MyStack {
+        Queue<Integer> queue;
+
+    public MyStack() {
+        queue = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        queue.add(x);
+        int size = queue.size();
+        while (size > 1) {
+            queue.add(queue.remove());
+            size--;
+        }
+    }
+    public int pop() {
+        return queue.remove();
+    }
+    
+    public int top() {
+        return queue.peek();
+    }
+    
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
